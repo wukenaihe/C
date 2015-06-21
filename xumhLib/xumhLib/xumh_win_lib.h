b015710dd8400880ece4dab90f8ef25420f03ed6
Xumh <wukenaihesos@gmail.com>
2015-06-21 19:05:18 +0800

    添加同步锁
    添加windows下的同步锁

--- xumhLib/xumhLib/xumh_win_lib.h
+++ xumhLib/xumhLib/xumh_win_lib.h
@@ -7,6 +7,32 @@
 namespace Xumh{
 	//根据错误码返回，错误描述
 	BOOL GetErrorMessage(DWORD code,std::wstring& message);
+
+
+	//同步锁，自动释放
+	//CRITICAL_SECTION lock;
+	//InitializeCriticalSection(&lock);
+	//DeleteCriticalSection(&lock);
+	class synchronize
+	{
+	private:
+		CRITICAL_SECTION* plock;
+
+	public:
+		synchronize(CRITICAL_SECTION* lock)
+		{
+			plock = lock;
+			if ( plock == NULL ) return;
+
+			EnterCriticalSection(plock);
+		}
+
+		~synchronize()
+		{
+			if ( plock != NULL )
+				LeaveCriticalSection(plock);
+		} 
+	};
 }
 
 #endif
